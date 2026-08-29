bc7af69d0435e6b1303bc96aa18592f66dc51250
JavapythonRust <ohmarsducky@gmail.com>
2026-08-29 17:20:38 -0400

    Update config.rs

--- tools/rust/arpgremlin/src/config.rs
+++ tools/rust/arpgremlin/src/config.rs
@@ -1,1 +1,18 @@
+use std::net::Ipv4Addr;
+use pnet::datalink::MacAddr;
 
+#[derive(Clone)]
+pub struct Config {
+    pub victim_ip: Ipv4Addr,
+    pub gateway_ip: Ipv4Addr,
+    pub victim_mac: MacAddr,
+    pub gateway_mac: MacAddr,
+    pub my_mac: MacAddr,
+}
+
+pub enum TxMessage {
+    Frame(Vec<u8>),
+    Poison,
+    Restore,
+    Shutdown,
+}
