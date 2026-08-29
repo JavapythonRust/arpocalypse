c081741139ae3b8bb36f5b74fc2438165bd17c2f
JavapythonRust <ohmarsducky@gmail.com>
2026-08-29 17:24:58 -0400

    Update arp.rs

--- tools/rust/arpgremlin/src/arp.rs
+++ tools/rust/arpgremlin/src/arp.rs
@@ -1,1 +1,16 @@
+use std::net::Ipv4Addr;
 
+use pnet::datalink::MacAddr;
+
+pub struct ArpInfo {
+    pub sender_mac: MacAddr,
+    pub sender_ip: Ipv4Addr,
+    pub target_mac: MacAddr,
+    pub target_ip: Ipv4Addr,
+}
+
+/*
+ * ARP parsing/construction helpers go here.
+ *
+ 
+ */
